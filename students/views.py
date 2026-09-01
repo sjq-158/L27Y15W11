c33aa46f53f41b51470dca148b82599f44a705b1
sjq-158 <erickafatimareignrligaray@gmail.com>
2026-09-01 15:41:36 +0800

    initial commit

---
+++ students/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Student
+ 
+def student_list(request):
+    students = Student.objects.all()
+    return render(request,
+                  "students/list.html",
+                  {"students": students})
